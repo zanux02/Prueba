8c45144becf07f93346a0b4a302f487f7ce79434
zanux02 <manuelbelmonteoliva@gmail.com>
2024-06-20 16:21:23 +0200

    prueba

--- .firebase/prueba-df7ab/hosting/flutter_service_worker.js
+++ .firebase/prueba-df7ab/hosting/flutter_service_worker.js
@@ -35,14 +35,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0068eacd4a1881c1eefec1f52fc84ad4
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "5d8c366bef9d278819bdcbd7613ed227",
+"flutter_bootstrap.js": "2093898d837f7c5753a38a10ea36bdaf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2abbe06f286fbddfc9c35fe417f76164",
-"/": "2abbe06f286fbddfc9c35fe417f76164",
-"main.dart.js": "181867af645fdfd78f442030916beef5",
+"index.html": "95773b88a10d8d7564911f31c095c293",
+"/": "95773b88a10d8d7564911f31c095c293",
+"main.dart.js": "cd58cf817416b4b096e630211783976c",
 "manifest.json": "7b9190a5279d484117cfef5d9ffb5a9e",
 "version.json": "4b8e6b5931ce70d94e4d0e167eddcbc8"};
 // The application shell files that are downloaded before a service worker can
--- lib/screens/alumnado/horario_detalles_alumnado_screen.dart
+++ lib/screens/alumnado/horario_detalles_alumnado_screen.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:kk/providers/alumnado_provider.dart';
+import 'package:kk/providers/profesor_provider.dart';
 import 'package:kk/models/horario_response.dart';
 
-class HorarioDetallesAlumnadoScreen extends StatelessWidget {
-  const HorarioDetallesAlumnadoScreen({Key? key}) : super(key: key);
+class HorarioProfesoresDetallesScreen extends StatelessWidget {
+  const HorarioProfesoresDetallesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final index = ModalRoute.of(context)!.settings.arguments as int;
-    final alumnadoProvider = Provider.of<AlumnadoProvider>(context);
-    final listadoAlumnos = alumnadoProvider.listadoAlumnos;
-    final listadoHorarios = alumnadoProvider.listadoHorarios;
+    final profesorProvider = Provider.of<ProfesorProvider>(context, listen: false);
+    final listadoProfesores = profesorProvider.listadoProfesor;
+    final listadoHorarios = profesorProvider.listadoHorarios;
 
-    // Obtenemos los dias
-    Set<String> diasUnicos = listadoHorarios.map((horario) => horario.dia.substring(0, 1)).toSet();
-    List<String> diasOrdenados = ["L", "M", "X", "J", "V"].where((dia) => diasUnicos.contains(dia)).toList();
-
-    // Obtenemos las horas ordeandas
+    // Crear lista de horas a partir de los horarios disponibles
     Set<String> horasUnicas = listadoHorarios.map((horario) => horario.hora).toSet();
     List<String> horasOrdenadas = horasUnicas.toList()
-      ..sort((a, b) => int.parse(a.split(":")[0]).compareTo(int.parse(b.split(":")[0])));
+      ..sort((a, b) => _compareHours(a, b));
+
+    // Crear lista de días a partir de los horarios disponibles
+    List<String> diasOrdenados = _getDiasOrdenados(listadoHorarios);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "HORARIO DE ${listadoAlumnos[index].curso} ",
+          "HORARIO DE ${listadoProfesores[index].nombre} ",
           style: const TextStyle(fontSize: 16),
         ),
       ),
@@ -33,13 +32,16 @@ class HorarioDetallesAlumnadoScreen extends StatelessWidget {
         decoration: const BoxDecoration(color: Colors.white),
         child: Column(
           children: [
-            Table(
-              border: TableBorder.all(style: BorderStyle.solid),
-              children: [
-                diasSemana(diasOrdenados),
-                for (int i = 0; i < horasOrdenadas.length; i++)
-                  diaHorario(context, index, listadoHorarios, i, diasOrdenados, horasOrdenadas),
-              ],
+            Container(
+              color: Colors.blue,
+              child: Table(
+                border: TableBorder.all(style: BorderStyle.solid),
+                children: [
+                  diasSemana(diasOrdenados),
+                  for (int i = 0; i < horasOrdenadas.length; i++)
+                    diaHorario(context, index, listadoHorarios, horasOrdenadas[i]),
+                ],
+              ),
             ),
           ],
         ),
@@ -68,26 +70,19 @@ class HorarioDetallesAlumnadoScreen extends StatelessWidget {
     return TableRow(children: widgetsDias);
   }
 
-  TableRow diaHorario(BuildContext context, int index, List<HorarioResult> listadoHorarios, int horaDia, List<String> diasOrdenados, List<String> horasOrdenadas) {
-    final alumnadoProvider = Provider.of<AlumnadoProvider>(context);
-    final listadoAlumnos = alumnadoProvider.listadoAlumnos;
-
-    // Obtenemos el horaria del curso
-    List<HorarioResult> cursoHorarios = listadoHorarios
-        .where((horario) => horario.curso == listadoAlumnos[index].curso)
-        .toList();
+  TableRow diaHorario(BuildContext context, int index, List<HorarioResult> listadoHorarios, String hora) {
 
     List<Widget> widgetsClases = [];
 
-    for (int numDia = 0; numDia < diasOrdenados.length; numDia++) {
+    for (int numDia = 0; numDia < _getDiasOrdenados(listadoHorarios).length; numDia++) {
       String asignatura = "";
       String aula = "";
 
-      for (int i = 0; i < cursoHorarios.length; i++) {
-        if (cursoHorarios[i].dia.startsWith(diasOrdenados[numDia]) &&
-            cursoHorarios[i].hora == horasOrdenadas[horaDia]) {
-          asignatura = cursoHorarios[i].asignatura;
-          aula = cursoHorarios[i].aulas;
+      for (int i = 0; i < listadoHorarios.length; i++) {
+        if (listadoHorarios[i].dia.startsWith(_getDiasOrdenados(listadoHorarios)[numDia]) &&
+            listadoHorarios[i].hora == hora) {
+          asignatura = listadoHorarios[i].asignatura;
+          aula = listadoHorarios[i].aulas;
           break;
         }
       }
@@ -116,7 +111,7 @@ class HorarioDetallesAlumnadoScreen extends StatelessWidget {
       Container(
         color: Colors.blue,
         child: Text(
-          _formatHourRange(horasOrdenadas[horaDia]),
+          _formatHourRange(hora),
           style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
           textAlign: TextAlign.center,
         ),
@@ -125,7 +120,30 @@ class HorarioDetallesAlumnadoScreen extends StatelessWidget {
     ]);
   }
 
+  List<String> _getDiasOrdenados(List<HorarioResult> listadoHorarios) {
+    Set<String> diasUnicos = listadoHorarios.map((horario) => horario.dia.substring(0, 1)).toSet();
+    List<String> diasOrdenados = ["L", "M", "X", "J", "V"].where((dia) => diasUnicos.contains(dia)).toList();
+    return diasOrdenados;
+  }
+
+  int _compareHours(String a, String b) {
+    // Comparar horas en formato "HH:mm"
+    final partsA = a.split(":");
+    final partsB = b.split(":");
+    final hourA = int.parse(partsA[0]);
+    final minuteA = int.parse(partsA[1]);
+    final hourB = int.parse(partsB[0]);
+    final minuteB = int.parse(partsB[1]);
+
+    if (hourA != hourB) {
+      return hourA.compareTo(hourB);
+    } else {
+      return minuteA.compareTo(minuteB);
+    }
+  }
+
   String _formatHourRange(String hour) {
+    // Formatear la hora en formato "HH:mm"
     final parts = hour.split(":");
     final startHour = int.parse(parts[0]);
     final startMinutes = int.parse(parts[1]);
@@ -133,6 +151,6 @@ class HorarioDetallesAlumnadoScreen extends StatelessWidget {
     final endHour = startHour + 1;
     final endMinutes = startMinutes == 30 ? "30" : "00";
 
-    return "$hour  -  $endHour:$endMinutes";
+    return "$hour - ${endHour.toString().padLeft(2, '0')}:$endMinutes";
   }
 }
