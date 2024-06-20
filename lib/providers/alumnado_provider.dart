96866effc678826c0375132bc90a81cf3b1dc884
zanux02 <manuelbelmonteoliva@gmail.com>
2024-06-20 09:53:04 +0200

    prueba

--- lib/providers/alumnado_provider.dart
+++ lib/providers/alumnado_provider.dart
@@ -53,7 +53,7 @@ class AlumnadoProvider extends ChangeNotifier {
 
   Future<void> getHorario() async {
     const url =
-        "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=14nffuLY-WILXuAQFMUWNEZIYK08WxI0g1_aK73Ths9Q&sheet=Horarios";
+        "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=11Y4M52bYFMCIa5uU52vKll2-OY0VtFiGK2PhMWShngg&sheet=Horarios";
     String jsonData = await Utilidades.getJsonData(url);
     jsonData = '{"results":$jsonData}';
     final cursosResponse = HorarioResponse.fromJson(jsonData);
