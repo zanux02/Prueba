3af95d95fc56727c1f861419561f3631d74636e7
zanux02 <manuelbelmonteoliva@gmail.com>
2024-06-21 12:21:16 +0200

    prueba

--- lib/screens/alumnado/localizacion_alumnado_screen.dart
+++ lib/screens/alumnado/localizacion_alumnado_screen.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:kk/models/alumnos_response.dart';
-import 'package:kk/models/horario_response.dart';
+import 'package:provider/provider.dart';
 
-void _mostrarAlert(BuildContext context, int index,
+import '../../models/models.dart';
+import '../../providers/alumnado_provider.dart';
+
+class LocalizacionAlumnadoScreen extends StatefulWidget {
+  const LocalizacionAlumnadoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LocalizacionAlumnadoScreen> createState() =>
+      _LocalizacionAlumnadoScreenState();
+}
+
+class _LocalizacionAlumnadoScreenState
+    extends State<LocalizacionAlumnadoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final alumnadoProvider = Provider.of<AlumnadoProvider>(context);
+    final listadoAlumnos = alumnadoProvider.listadoAlumnos;
+    final listadoHorarios = alumnadoProvider.listadoHorarios;
+
+    List<DatosAlumnos> listaOrdenada = [];
+    listaOrdenada.addAll(listadoAlumnos);
+
+    listaOrdenada.sort((a, b) => a.nombre.compareTo(b.nombre));
+    return Scaffold(
+      appBar: AppBar(title: const Text("LOCALIZACION ALUMNOS")),
+      body: ListView.builder(
+        itemCount: listaOrdenada.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              _mostrarAlert(context, index, listaOrdenada, listadoHorarios);
+            },
+            child: ListTile(
+              title: Text(listaOrdenada[index].nombre),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context, int index,
       List<DatosAlumnos> listaAlumnos, List<HorarioResult> listadoHorarios) {
     DateTime ahora = DateTime.now();
 
@@ -26,7 +66,8 @@ void _mostrarAlert(BuildContext context, int index,
       if (dia == 1) {
         listadoHorarioCursoDia.add(listadoHorarioCurso[i]);
       } else {
-        listadoHorarioCursoDia.add(listadoHorarioCurso[i + (6 * (dia - 1))]);
+        listadoHorarioCursoDia
+            .add(listadoHorarioCurso[i + (6 * (dia - 1))]);
       }
     }
 
@@ -41,9 +82,10 @@ void _mostrarAlert(BuildContext context, int index,
       int endHour = int.parse(horaFin.split(":")[0]);
       int endMinute = int.parse(horaFin.split(":")[1]);
 
-      // Comparar la hora actual con la hora de inicio y fin de la asignatura
-      if ((horaActual > startHour || (horaActual == startHour && minutosActual >= startMinute)) &&
-          (horaActual < endHour || (horaActual == endHour && minutosActual < endMinute))) {
+      // Verificar si el alumno está en clase en este momento
+      if (horaActual == startHour &&
+          minutosActual >= startMinute &&
+          (horaActual < endHour || (horaActual == endHour && minutosActual <= endMinute))) {
         asignatura = listadoHorarioCursoDia[i].asignatura;
         aula = listadoHorarioCursoDia[i].aulas;
         alumnoEnClase = true;
@@ -59,37 +101,38 @@ void _mostrarAlert(BuildContext context, int index,
     }
 
     showDialog(
-        context: context,
-        barrierDismissible: true,
-        builder: (context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
-            title: Text(listaAlumnos[index].nombre),
-            content: Text(texto),
-            actions: [
-              TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text("Cerrar")),
-            ],
-          );
-        });
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text(listaAlumnos[index].nombre),
+          content: Text(texto),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cerrar"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   String _calculateEndTime(String horaInicio) {
+    // Parsear la hora de inicio
     final parts = horaInicio.split(":");
     int startHour = int.parse(parts[0]);
     int startMinute = int.parse(parts[1]);
 
-    // Calcular la hora de fin
-    int endHour = startHour;
-    int endMinute = startMinute == 30 ? 0 : 30;
-    if (startMinute == 30) {
-      endHour++;
-    }
+    // Calcular la hora de fin sumando 1 hora
+    int endHour = startHour + 1;
 
+    // Formatear la hora de fin
     String formattedEndHour = endHour.toString().padLeft(2, '0');
-    String formattedEndMinute = endMinute.toString().padLeft(2, '0');
+    String formattedEndMinute = startMinute.toString().padLeft(2, '0');
 
     return "$formattedEndHour:$formattedEndMinute";
   }
+}
