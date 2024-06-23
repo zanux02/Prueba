4f175ba3fc73e406fb4de5133b39fb882ca20631
zanux02 <manuelbelmonteoliva@gmail.com>
2024-06-23 17:38:29 +0200

    prueba

--- .firebase/prueba-df7ab/hosting/flutter_bootstrap.js
+++ .firebase/prueba-df7ab/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "949398471"
+    serviceWorkerVersion: "3204850119"
   }
 });
--- lib/screens/servicio/servicio_informes_detalles_screen.dart
+++ lib/screens/servicio/servicio_informes_detalles_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iJandula/models/servicio_response.dart';
 import 'package:provider/provider.dart';
 import 'package:iJandula/models/models.dart';
 import 'package:iJandula/providers/providers.dart';
-//import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class ServicioInformesDetallesScreen extends StatelessWidget {
@@ -21,38 +21,47 @@ class ServicioInformesDetallesScreen extends StatelessWidget {
         alumno.add(listaAlumnos[i]);
       }
     }
+
     final listadoAlumnosDetalles = servicioProvider.listadoAlumnosServicio;
 
-    List<String> fechas = [];
-
-    for (int i = 0; i < listadoAlumnosDetalles.length; i++) {
-      if (listadoAlumnosDetalles[i].nombreAlumno == nombreParametro) {
-        fechas.add(
-            "${listadoAlumnosDetalles[i].fechaEntrada} - ${listadoAlumnosDetalles[i].fechaSalida}");
-      }
-    }
+    // Filtrar los servicios del alumno seleccionado
+    List<Servicio> serviciosAlumno = listadoAlumnosDetalles
+        .where((servicio) => servicio.nombreAlumno == nombreParametro)
+        .toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(nombreParametro.toString().toUpperCase()),
       ),
       body: SafeArea(
-        child: Stack(children: [
-          ListView.builder(
-            itemCount: fechas.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text(fechas[index]),
-              );
-            },
-          ),
-          Container(
+        child: Stack(
+          children: [
+            ListView.builder(
+              itemCount: serviciosAlumno.length,
+              itemBuilder: (BuildContext context, int index) {
+                Servicio servicio = serviciosAlumno[index];
+
+                // Formatear las fechas y horas
+                String fechaEntradaHoraEntrada =
+                    '${servicio.fechaEntrada} ${servicio.horaEntrada}';
+                String fechaSalidaHoraSalida =
+                    '${servicio.fechaSalida} ${servicio.horaSalida}';
+
+                return ListTile(
+                  title: Text('$fechaEntradaHoraEntrada - $fechaSalidaHoraSalida'),
+                );
+              },
+            ),
+            Container(
               padding: const EdgeInsets.only(right: 20, bottom: 20),
               alignment: Alignment.bottomRight,
               child: FloatingActionButton(
-                  onPressed: () => _mostrarAlert(context, alumno),
-                  child: const Icon(Icons.person)))
-        ]),
+                onPressed: () => _mostrarAlert(context, alumno),
+                child: const Icon(Icons.person),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -65,87 +74,84 @@ class ServicioInformesDetallesScreen extends StatelessWidget {
     String mailAlumno = alumno[0].email;
 
     showDialog(
-        context: context,
-        barrierDismissible: true,
-        builder: (context) {
-          TextStyle textStyle = const TextStyle(fontWeight: FontWeight.bold);
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        TextStyle textStyle = const TextStyle(fontWeight: FontWeight.bold);
 
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
-            title: Text(alumno[0].nombre),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                ),
-                Row(
-                  children: [
-                    Text("Correo: ", style: textStyle),
-                    Text(mailAlumno),
-                    IconButton(
-                        onPressed: () {
-                          launchUrlString("mailto: $mailAlumno");
-                        },
-                        icon: const Icon(Icons.mail),
-                        color: Colors.blue),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Teléfono Alumno: ",
-                      style: textStyle,
-                    ),
-                    Text("$numeroTlfAlumno"),
-                    IconButton(
-                        onPressed: () {
-                          launchUrlString("tel:$numeroTlfAlumno");
-                        },
-                        icon: const Icon(Icons.phone),
-                        color: Colors.blue),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Teléfono Padre: ",
-                      style: textStyle,
-                    ),
-                    Text("$numeroTlfPadre"),
-                    IconButton(
-                        onPressed: () {
-                          launchUrlString("tel:$numeroTlfPadre");
-                        },
-                        icon: const Icon(Icons.phone),
-                        color: Colors.blue),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Teléfono Madre: ",
-                      style: textStyle,
-                    ),
-                    Text("$numeroTlfMadre"),
-                    IconButton(
-                        onPressed: () {
-                          launchUrlString("tel:$numeroTlfMadre");
-                        },
-                        icon: const Icon(Icons.phone),
-                        color: Colors.blue)
-                  ],
-                )
-              ],
-            ),
-            actions: [
-              TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text("Cerrar")),
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text(alumno[0].nombre),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Divider(
+                color: Colors.black,
+                thickness: 1,
+              ),
+              Row(
+                children: [
+                  Text("Correo: ", style: textStyle),
+                  Text(mailAlumno),
+                  IconButton(
+                    onPressed: () {
+                      launchUrlString("mailto: $mailAlumno");
+                    },
+                    icon: const Icon(Icons.mail),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Teléfono Alumno: ", style: textStyle),
+                  Text("$numeroTlfAlumno"),
+                  IconButton(
+                    onPressed: () {
+                      launchUrlString("tel:$numeroTlfAlumno");
+                    },
+                    icon: const Icon(Icons.phone),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Teléfono Padre: ", style: textStyle),
+                  Text("$numeroTlfPadre"),
+                  IconButton(
+                    onPressed: () {
+                      launchUrlString("tel:$numeroTlfPadre");
+                    },
+                    icon: const Icon(Icons.phone),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Teléfono Madre: ", style: textStyle),
+                  Text("$numeroTlfMadre"),
+                  IconButton(
+                    onPressed: () {
+                      launchUrlString("tel:$numeroTlfMadre");
+                    },
+                    icon: const Icon(Icons.phone),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
             ],
-          );
-        });
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cerrar"),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
